a538f559733bdd84443adf568d22ed9bc7e831dd
cfif bdfyjd <lan467@mail.ru>
2019-04-22 17:36:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp9/Complex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp9
+{
+    class Complex
+    {
+        double module;
+        double argument;
+
+        public Complex(double module, double argument)
+        {
+            this.module = module;
+            this.argument = argument;
+        }
+
+        public double Module { get => module; set => module = value; }
+        public double Argument { get => argument; set => argument = value; }
+
+        public override string ToString()
+        {
+            return "Тригонометрическая форма: " + module.ToString() + "*(Cos(" + argument.ToString() + ")+i*Sin(" + argument.ToString() + "))" + "\n" +
+                "Алгебраическая форма: " + Math.Round(this.module * Math.Cos(this.argument), 2) + "+" + Math.Round(this.module * Math.Sin(this.argument), 2) + "i";
+
+        }
+
+        public static Complex umn(Complex x, Complex y)
+        {
+            return new Complex(x.Module * y.Module, x.Argument + y.Argument);
+        }
+
+        public static Complex del(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module / y.Module, 2), x.Argument - y.Argument);
+        }
+
+        public static Complex plu(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module * Math.Cos(x.Argument), 2) + Math.Round(y.Module * Math.Cos(y.Argument), 2), Math.Round(x.Module * Math.Sin(x.Argument), 2) + Math.Round(y.Module * Math.Sin(y.Argument), 2));
+        }
+
+        public static Complex min(Complex x, Complex y)
+        {
+            return new Complex(Math.Round(x.Module * Math.Cos(x.Argument), 2) - Math.Round(y.Module * Math.Cos(y.Argument), 2), Math.Round(x.Module * Math.Sin(x.Argument), 2) - Math.Round(y.Module * Math.Sin(y.Argument), 2));
+        }
+
+    }
+}
